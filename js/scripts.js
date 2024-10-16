49ca402352d50a61578f7567c308d36b76c72935
MacarenaCarrero <macarena.c.rios@gmail.com>
2024-10-16 23:42:47 +0200

    JavaScript-2

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function adult (name, age){
+    if(age >= 18){
+        console.log ('Hola ' + name + ' eres mayor de edad ');
+    } else if (age <18){
+        console.log ('Hola ' + name + ' eres menor de edad ')
+    }
+    }
+  
+adult('Macarena' , 19 );
+adult('Fulanito', 15)
